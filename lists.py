13dd667b81032013cde94c25927cde48c3a499aa
ThinkTrout <chengxi.li.usa@gmail.com>
2024-09-08 13:52:43 -0400

    Fixed dictionaries and inputs

---
+++ lists.py
@@ -0,0 +1,114 @@
+FEO = 'FEO'
+FE = 'FE'
+O = 'O'
+C = 'C'
+FLX = 'FLX'
+EPO = 'EPO'
+H = 'H'
+CL = 'CL'
+LST = 'LST'
+
+HB1 = {
+    "name": "HB1",
+    "BSE": 2,
+    'BBH': 4,
+    'BDE': 2,
+    'BTA': 1,
+    'MCG': 40,
+    'area': 10,
+}
+
+HB2 = {
+    "name": "HB2",
+    "BSE": 2,
+    'BBH': 2,
+    'BDE': 2,
+    'BTA': 2,
+    'TRU': 2,
+    'MCG': 48,
+    'area': 12
+}
+
+COL = {
+    "name": "COL",
+    "BSE": 16,
+    'MCG': 60,
+    'area': 15,
+    'per': 3,
+    'item': 'O',
+    'hours': 8,
+    'minutes': 17,
+    'pioneers': 60
+}
+
+EXTFEO = {
+    "name": "EXT",
+    "BSE": 16,
+    'MCG': 60,
+    'area': 25,
+    'per': 8,
+    'item': 'FEO',
+    'hours': 15,
+    'minutes': 12,
+    'pioneers': 60
+}
+
+EXTLST = {
+    "name": "EXTLST",
+    "BSE": 16,
+    'MCG': 60,
+    'area': 25,
+    'per': 11,
+    'item': 'LST',
+    'hours': 15,
+    'minutes': 8,
+    'pioneers': 60
+}
+
+SME = {
+    "name": "SME",
+    "BSE": 6,
+    'BBH': 4,
+    'BDE': 4,
+    'MCG': 68,
+    'area': 17,
+    'per': 4,
+    'item': 'FE',
+    'hours': 16,
+    'minutes': 49,
+    'pioneers': 50
+}
+
+POL = {
+    "name": "POL",
+    "BSE": 4,
+    'BDE': 4,
+    'LBH': 8,
+    'MCG': 60,
+    'TRU': 2,
+    'area': 15,
+    'per': 50,
+    'item': 'EPO',
+    'hours': 24,
+    'minutes': 0,
+    'pioneers': 10,
+    'settlers': 25,
+    'cost': [1,1,1,1]
+}
+
+CHP = {
+    "name": "CHP",
+    "BSE": 3,
+    'BBH': 3,
+    'BDE': 3,
+    'MCG': 72,
+    'TRU': 4,
+    'area': 18,
+    'per': 10,
+    'item': 'FLX',
+    'hours': 12,
+    'minutes': 0,
+    'pioneers': 20,
+    'settlers': 60,
+    'cost': [1],
+}
--- main.py
+++ main.py
@@ -1,69 +1,102 @@
 from colors import *
+from lists import *
 
-FEO = 'FEO'
-FE = 'FE'
-LST = 'LST'
-O = 'O'
-C = 'C'
-BBH = 'BBH'
-FLX = 'FLX'
+def prodRate(building_count, building,cost1=None, cost2=None, cost3=None, cost4=None):
+    pioneers.append(building.get('pioneers', 0) * building_count)
+    settlers.append(building.get('settlers', 0) * building_count)
 
-dict = {
-    FEO: [8],
-    LST: [11],
-    O: [3],
-    FE: [4,6,1,1,1],
-    BBH: [1,1,2],
-    FLX: [10],
-}
+    hrs = building.get('hours')
+    mins = building.get('minutes')
+    totalprod = building.get('per') * building_count
+    item = building.get('item')
+    per = building.get('per')
 
-# ---------------------------
-
-def prodRate(item, building, hrs, mins, cost1=None, cost2=None, cost3=None, cost4=None, days=None, price=None):
-    per = dict[item][0]
     exthrs = hrs + mins / 60
-    totalprod = per * building
+    totalprod = per * building_count
 
-    if days is not None:
-        cycles = int(days * 24 / exthrs)
-        period = f'in {days} days'
-    else:
-        cycles = int(168 / exthrs)
-        period = 'per week'
+    cycles = int(168 / exthrs)
+    period = 'per week'
 
     perweek = totalprod * cycles
     cost_str = ''
-    
+
     if cost1 is not None:
-        cost_1 = dict[item][1] * cycles * building
+        cost_1 = building.get('cost')[0] * cycles * building_count
         cost_str += f', costing {cost_1} {cost1}'
     if cost2 is not None:
-        cost_2 = dict[item][2] * cycles * building
+        cost_2 = building.get('cost')[1] * cycles * building_count
         cost_str += f', {cost_2} {cost2}'
     if cost3 is not None:
-        cost_3 = dict[item][3] * cycles * building
+        cost_3 = building.get('cost')[2] * cycles * building_count
         cost_str += f', {cost_3} {cost3}'
     if cost4 is not None:
-        cost_4 = dict[item][4] * cycles * building
+        cost_4 = building.get('cost')[3] * cycles * building_count
         cost_str += f', {cost_4} {cost4}'
 
-    if price is not None:
-        totalprice = price*perweek
-        price_str = f', selling at {totalprice} NCC in total'
-    else:
-        price_str = ''
+    construction(building,building_count)
+    total_area.append(building.get('area')*building_count)
+    print(f'{item}: {building_count} {building.get("name")} * {per} every {hrs}h{mins}m, {NEON_BLUE}{perweek}{WHITE} {period}{cost_str}. Takes up {NEON_YELLOW}{building.get("area")*building_count}{WHITE} area.')
 
-    print(f'{item}: {totalprod} every {hrs}h{mins}m, {perweek} {period}{cost_str}{price_str}.\n')
+def housing(building_count, building):
+    if building == HB1:
+        worker = "pioneers"
+    elif building == HB2:
+        worker = "settlers"
 
+    construction(building,building_count)
+    total_area.append(building.get('area')*building_count)
+    print(f"{building_count} {building.get('name')}. Space for {NEON_GREEN}{100*building_count}{WHITE} {worker}. Takes up {NEON_YELLOW}{building.get("area")*building_count}{WHITE} area.")
+
+def construction(building,building_count):
+    totalBSE.append(building.get("BSE", 0) * building_count)
+    totalBBH.append(building.get("BBH", 0) * building_count)
+    totalBDE.append(building.get("BDE", 0) * building_count)
+    totalMCG.append(building.get("MCG", 0) * building_count)
+    totalBTA.append(building.get("BTA", 0) * building_count)
+    totalTRU.append(building.get("TRU", 0) * building_count)
+    totalLBH.append(building.get("LBH", 0) * building_count)
 # --------------------------
+pioneers = []
+settlers = []
+totalBSE = []
+totalBBH = []
+totalBDE = []
+totalMCG = []
+totalBTA = []
+totalTRU = []
+totalLBH = []
+total_area = [25]
+'''
+print()
+prodRate(4,COL)
+prodRate(30,POL,C,H,CL,O)
 
-print("Welcome to the VELLICHOR Accounting Department.")
-print("This is Montem.\n")
+print()
+housing(6,HB1)
+print(f"                 {NEON_CYAN}{sum(pioneers)}{WHITE} total pioneers\n")
+housing(8, HB2)
+print(f"                 {NEON_CYAN}{sum(settlers)}{WHITE} total settlers\n")
 
-print(f'\n{BOLD}{ITALIC}Current production/extraction rates:{WHITE}\n')
+print(f"{NEON_YELLOW}{sum(total_area)} area used.")
+print(f"{RED}{750 - sum(total_area)} area left.")
 
-prodRate(FEO,1,15,58)
-prodRate(LST,1,15,54)
-prodRate(O,1,8,1)
-prodRate(FLX,1,26,0)
-prodRate(FE, 3, 16, 33, FEO, C, O, FLX)
+print("\nMaterial Costs:")
+print(
+    f"{NEON_PURPLE}{sum(totalBSE)}{WHITE} BSE\n"
+    f"{NEON_PURPLE}{sum(totalBBH)}{WHITE} BBH\n"
+    f"{NEON_PURPLE}{sum(totalBDE)}{WHITE} BDE\n"
+    f"{NEON_PURPLE}{sum(totalBTA)}{WHITE} BTA\n"
+    f"{NEON_PURPLE}{sum(totalTRU)}{WHITE} TRU\n"
+    f"{NEON_PURPLE}{sum(totalMCG)}{WHITE} MCG\n"
+    f"{NEON_PURPLE}{sum(totalLBH)}{WHITE} LBH\n"
+)
+
+totalmaterialcost = (sum(totalBSE)*1700 +
+                     sum(totalBBH)*2500 +
+                     sum(totalBDE)*2400 +
+                     sum(totalBTA)*1500 +
+                     sum(totalTRU)*400 +
+                     sum(totalMCG)*40 +
+                     sum(totalLBH)*4000)
+
+print(f"FINAL CALCULATION: {RED}{totalmaterialcost} NCC required.")'''
